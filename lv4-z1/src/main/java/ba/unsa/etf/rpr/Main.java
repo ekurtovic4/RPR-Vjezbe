5d5756953fe67fccd03b72222a15af42a54965cc
ekurtovic4 <148075994+ekurtovic4@users.noreply.github.com>
2023-11-10 17:14:37 +0100

    Dodan LaptopDaoJSONFile

--- lv4-z1/src/main/java/ba/unsa/etf/rpr/Main.java
+++ lv4-z1/src/main/java/ba/unsa/etf/rpr/Main.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-
+        LaptopDaoJSONFile lj = new LaptopDaoJSONFile();
+        lj.dodajLaptopUFile(new Laptop("b", "m", 100, 1, 2, 3, "p", "g", 10));
+        lj.dodajLaptopUFile(new Laptop("b1", "m1", 101, 11, 21, 31, "p1", "g1", 11));
+        ArrayList<Laptop> lista = lj.vratiPodatkeIzDatoteke();
+        System.out.println(lista.size());
+        for (Laptop laptop : lista)
+            System.out.println(laptop.getBrend());
     }
 }
