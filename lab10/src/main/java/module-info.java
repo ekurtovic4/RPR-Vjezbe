cc57b10ccc00720cc24997ed09fcda6f7621d10e
ekurtovic4 <148075994+ekurtovic4@users.noreply.github.com>
2023-12-31 16:56:13 +0100

    uradjena prva 3 zadatka

---
+++ lab10/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.lab10 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.lab10 to javafx.fxml;
+    exports com.example.lab10;
+    exports DAL;
+}
